7c3953868f8ee6b774f032d16dcb6e36a711609b
Vincent Li <vincent.mc.li@gmail.com>
2014-02-11 14:50:01 -0800

    Add tcp echo server receive and send timeout on child socket Sending RST when socket close with linger socket option

--- tcpcliserv/tcpserv01.c
+++ tcpcliserv/tcpserv01.c
@@ -7,6 +7,26 @@ main(int argc, char **argv)
 	pid_t				childpid;
 	socklen_t			clilen;
 	struct sockaddr_in	cliaddr, servaddr;
+	int opt;
+	int nsecs = 360;
+	while ((opt = getopt(argc, argv, "t:")) != -1) {
+		switch (opt) {
+		case 't':
+			nsecs = atoi(optarg);
+			break;
+		default: /* '?' */
+			fprintf(stderr, "Usage: %s [-t nsecs]\n",
+			argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+
+	struct linger ling;
+        struct timeval timeout;
+        timeout.tv_sec = nsecs;
+        timeout.tv_usec = 0;
+
 
 	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
@@ -22,6 +42,17 @@ main(int argc, char **argv)
 	for ( ; ; ) {
 		clilen = sizeof(cliaddr);
 		connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
+                ling.l_onoff = 1;
+                ling.l_linger = 0;
+                if (setsockopt(connfd, SOL_SOCKET, SO_LINGER, &ling, sizeof(ling)) < 0) {
+                         perror("setsockopt error");
+                }
+                if (setsockopt(connfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0) {
+                         perror("setsockopt error");
+                }
+                if (setsockopt(connfd, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout)) < 0) {
+                         perror("setsockopt error");
+                }
 
 		if ( (childpid = Fork()) == 0) {	/* child process */
 			Close(listenfd);	/* close listening socket */
